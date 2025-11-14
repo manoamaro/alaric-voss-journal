6c2aab2d425d0e3dcff9f396f3f675911b4a2bd8
Manoel Amaro <manoel.dealbuquerqueliraamaro@erstegroup.com>
2025-11-14 12:04:59 +0100

    vault backup: 2025-11-14 12:04:59

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Alaric Voss' Barovian Journal",
+    pageTitle: "Alaric Voss' Journal",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
